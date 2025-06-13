500793c23241a5cb272bcd1390dc58c6e782d942
khcmh2502 <khcmh2502@gmail.com>
2025-06-13 10:04:06 +0900

    cors 인가 규칙 설정 추가

--- boardProject-boot/src/main/java/edu/kh/project/common/config/SecurityConfig.java
+++ boardProject-boot/src/main/java/edu/kh/project/common/config/SecurityConfig.java
@@ -38,24 +38,24 @@ public class SecurityConfig {
 
         // HTTP 요청에 대한 인가(Authorization) 규칙 설정
         http.authorizeHttpRequests(auth -> auth
-                // "/admin/**" 경로의 요청은 인증된 사용자만 접근 가능. (예: JWT 토큰 등을 통해 인증)
+        		// '/admin/login' 경로는 인증 없이 누구나 접근 가능하도록 설정(인증 전 이므로)
+        		.requestMatchers("/admin/login").permitAll() 
+        		// "/admin/**" 경로의 요청은 인증된 사용자만 접근 가능. (ex. JWT 토큰 등을 통해 인증)
                 .requestMatchers("/admin/**").authenticated()
                 // "/testSock/**", "/chattingSock/**" 경로의 요청은 누구나 접근 가능.
                 .requestMatchers("/testSock/**", "/chattingSock/**").permitAll()
                 // 위에서 정의되지 않은 그 외 모든 요청은 누구나 접근 가능.
-                .anyRequest().permitAll()
-        );
-
-        // CSRF(Cross-Site Request Forgery) 보호 설정
-        // CSRF 보호를 활성화하고, 특정 요청에 대해 보호를 적용하는 Matcher를 지정
-        // form 기반 인증 시 CSRF 토큰을 사용하여 요청의 위변조를 방지
-        http.csrf(csrf -> csrf.requireCsrfProtectionMatcher(new CsrfProtectionMatcher()));
+                .anyRequest().permitAll())
+		        // CSRF(Cross-Site Request Forgery) 보호 설정
+		        // CSRF 보호를 활성화하고, 특정 요청에 대해 보호를 적용하는 Matcher를 지정
+		        // form 기반 인증 시 CSRF 토큰을 사용하여 요청의 위변조를 방지
+                .csrf(csrf -> csrf.requireCsrfProtectionMatcher(new CsrfProtectionMatcher()));
+      
 
         // X-Frame-Options 헤더 설정
-        // <frame>, <iframe>, <embed>, <object> 태그 내에서 렌더링될지 여부를 제어함.
-        // frameOptions.sameOrigin(): 같은 도메인 내(ex. cmh-boardproject.store)에서는 iframe 사용을 허용하지만,
-        // 외부 사이트에서 우리 페이지를 iframe으로 가져가는 것은 막음.
-        // 클릭재킹(Clickjacking)과 같은 공격을 방지하는 데 도움이 됨
+        // 'deny' 대신 'sameOrigin' 정책을 사용Add commentMore actions
+     	// 외부 사이트에서 우리 페이지를 iframe으로 가져가는 것은 막되, 
+     	// 같은 도메인 내(ex. cmh-boardproject.store)에서는 iframe 사용을 허용하는 정책
         http.headers(headers -> headers.frameOptions(frameOptions
                 -> frameOptions.sameOrigin()
         ));
